4594bf1b628d4dbd9d87192652ed20412211065a
ddodoi <happycat129@naver.com>
2025-01-18 22:31:28 +0900

    ✨ Feat: svg파일을 컴포넌트로 사용 가능하도록 기능 추가

---
+++ src/assets/svg/Copy.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const Copy = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="#000"
+      fillRule="evenodd"
+      d="M11 10a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h9a1 1 0 0 0 1-1v-9a1 1 0 0 0-1-1zm-3 1a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="#000"
+      fillRule="evenodd"
+      d="M4 3a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h1a1 1 0 1 1 0 2H4a3 3 0 0 1-3-3V4a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v1a1 1 0 1 1-2 0V4a1 1 0 0 0-1-1z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default Copy;
