46166ba92241398b4058d551afb624587047ab63
ddodoi <happycat129@naver.com>
2025-01-22 22:12:33 +0900

    ✨ Feat: path-alias 기능 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,20 @@ export default defineConfig({
     react(),
     svgr()
   ],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@api': '/src/api',
+      '@assets':'/src/assets',
+      '@components': '/src/components',
+      '@common': '/src/components/common',
+      '@constants/*': '/src/constants',
+      '@hooks/*':'/src/hooks',
+      '@models/*':  '/src/models',
+      '@pages/*': '/src/pages',
+      '@store/*': '/src/store',
+      '@styles/*': '/src/styles',
+      '@utils/*': '/src/utils',
+    },
+  },
 });
