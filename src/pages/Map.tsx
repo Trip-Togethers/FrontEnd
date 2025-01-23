d70966726db68e3e9db9086a1af114392941bd15
Galaxy-104 <js_park95@naver.com>
2025-01-23 13:59:48 +0900

    ✨ Feat: 구글지도 컴포넌트 추가 및 구현
    구글지도 api를 요청하여 웹페이지에 구글지도를 생성하는 기능을 구현하였습니다.

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,14 +1,30 @@
 import { styled } from "styled-components";
+import GoogleMapComponent from "../components/maps/GoogleMap";
+import Sidebar from "../components/maps/Sidebar";
+import { DEFAULT_LOCATION } from "../constants/location";
 
 function Map() {
 	return (
 		<MapStyle>
-				Map
+				{/* 사이드바 */}
+				<Sidebar />
+
+				{/* 사이드바 상세 */}
+				{/* 검색 카테고리 */}
+
+				{/* 지도 */}
+				<GoogleMapComponent 
+					latitude={DEFAULT_LOCATION.lat}
+					longitude={DEFAULT_LOCATION.lng}
+				/>
 		</MapStyle>
 	);
 }
 
 const MapStyle = styled.div`
+	display: flex;
+	width: 100vw;
+	height: 100vh;
 `;
 
 export default Map;
