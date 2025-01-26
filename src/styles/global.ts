2e84e00fd085b038d8fd055ab9c29104e0cd5dc8
YEONGINKWON <duddls745@nate.com>
2025-01-26 19:19:45 +0900

    Feat: theme과 globalstyle추가, svg로고 추가

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,43 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 import "sanitize.css";
-import "./font.css"
 
 export const GlobalStyle = createGlobalStyle`
-    * {
-        font-family: 'BMJUA'; 
-        }
-    html, body {
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-  }
+    body{
+        padding : 0;
+        margin : 0;
+    }
 `;
-
-export const GlobalStyle2 = createGlobalStyle`
-        * {
-        font-family: 'JalnanGothic'; 
-        }
-    
-    html, body {
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-  }
-`;
-
-export const GlobalStyle3 = createGlobalStyle`
-        * {
-    font-family: 'SBAggroB'; 
-        }
-
-    html, body {
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-  }
-`;
-
-
