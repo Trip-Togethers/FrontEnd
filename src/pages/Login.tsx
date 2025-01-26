ef15ddb00ac2bec40a72bafa75b96211f274d703
YEONGINKWON <duddls745@nate.com>
2025-01-26 19:09:10 +0900

    ✨Feat: 페이지 파일 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,31 +1,11 @@
 import { styled } from "styled-components";
-import { ModalPlanButton, AddPostButton, WithdrawButton, JoinButton, CheckButton, EditButton, CreatePlanButton, DeleteButton, CancelButton, LoginButton } from '@/components/common/Button';
-import { PostContentInput ,PostTitleInput,AddPlanInput, SearchInput ,EmailInput, PasswordInput, NicknameInput, ContactInput  } from "@/components/common/InputText" 
-
 
 function Login() {
-    return (
-        <LoginStyle>
-            <CreatePlanButton />
-            <DeleteButton />
-            <CancelButton />
-            <LoginButton />
-            <ModalPlanButton />
-            <EditButton />
-            <CheckButton />
-            <JoinButton />
-            <WithdrawButton />
-            <AddPostButton />
-            <EmailInput />
-            <PasswordInput />
-            <NicknameInput />
-            <ContactInput />
-            <SearchInput />
-            <AddPlanInput />
-            <PostTitleInput />
-            <PostContentInput />
-        </LoginStyle>
-    );
+	return (
+		<LoginStyle>
+				Login
+		</LoginStyle>
+	);
 }
 
 const LoginStyle = styled.div`
