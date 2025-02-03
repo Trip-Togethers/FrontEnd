917a9cb879d33b5f54b227acd4c28dde4a1e7679
ddodoi <happycat129@naver.com>
2025-02-03 13:41:16 +0900

    ✨ Feat: 일정 상세 페이지 초반 구현

---
+++ src/components/detail/Ticket.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import Logo from "@assets/svg/Logo.tsx";
+
+const Ticket = () => {
+  return (
+    <div>
+      <TicketStyle>
+        <div>
+          <Logo className="logo" />
+        </div>
+        <div>Ticket</div>
+      </TicketStyle>
+    </div>
+  );
+};
+
+const TicketStyle = styled.div`
+  display: grid;
+  grid-template-columns: 200px 200px 200px;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 4px 5px 0px;
+  height: 100%;
+  .logo {
+    overflow: inherit;
+    transform: rotate(-90deg);
+    fill: white;
+    background-color: ${({ theme }) => theme.color.primary_green};
+    padding: 0.5rem;
+  }
+`;
+
+export default Ticket;
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,9 +1,22 @@
 import { styled } from "styled-components";
+import Ticket from "@/components/detail/ticket";
+import Modal from "@/components/common/modal";
+import { useState } from "react";
 
 function Detail() {
-  return <DetailStyle>Detail</DetailStyle>;
+  const { isIngOpen, setIngOpen } = useState(false);
+  return (
+    <DetailStyle>
+      <Ticket />
+      <Modal isOpen={isIngOpen}>제목:</Modal>
+      <div>dmdrks</div>
+    </DetailStyle>
+  );
 }
 
-const DetailStyle = styled.div``;
+const DetailStyle = styled.div`
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 4px 5px 0px;
+  border: 10px;
+`;
 
 export default Detail;
